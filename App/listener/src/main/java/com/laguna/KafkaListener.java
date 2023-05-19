df8b121857714ded6134681f46c712d4f0dc4071
Nestor <nmorabonilla@gmail.com>
2023-05-19 09:51:29 -0600

    Updated KafkaListener

---
+++ App/listener/src/main/java/com/laguna/KafkaListener.java
@@ -0,0 +1,37 @@
+import java.util.Collections;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+public class KafkaListener {
+
+    private final static String TOPIC = "countries";
+    private final static String BOOTSTRAP_SERVERS = "localhost:9092";
+
+    public void start(){
+        final Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "KafkaConsumer");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        // Create the consumer using props
+        final Consumer<String, String> consumer = new KafkaConsumer<>(props);
+
+        // Subscribe to the topic
+        consumer.subscribe(Collections.singletonList(TOPIC));
+
+        // Poll for new messages
+        while (true) {
+            final ConsumerRecords<String, String> records = consumer.poll(1000);
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+            }
+        }
+    }
+}
